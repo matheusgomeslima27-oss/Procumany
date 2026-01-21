3f55298023ccf1eeeeb6cf479c0a94f930b7c480
matheusgomeslima27-oss <matheus.gomes.lima27@escola.pr.gov.br>
2026-01-21 19:37:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
-// Token que você vai colocar no Meta depois
-const VERIFY_TOKEN = "meubotinsta123";
+// Agora o token vem da Render
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-// Rota do webhook (é essa que você coloca no Meta)
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("Webhook verificado com sucesso!");
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
   }
 });
 
-// Para a Render saber que seu servidor está vivo
 app.get("/", (req, res) => {
   res.send("Bot do Instagram está rodando!");
 });
